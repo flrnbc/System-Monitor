8d89ec9c9cfdb5dd08a683bbf4fbcb230660b117
Florian Beck <flrn.beck@gmail.com>
2021-07-20 18:08:00 +0200

    Add format function.

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,29 @@
 #include <string>
-
+#include <iomanip> // for std::setw, std::setfill
+#include <sstream>
 #include "format.h"
 
 using std::string;
 
-// TODO: Complete this helper function
+// DONE: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+
+// helper function
+string PadZeros(long n, int k) {
+    std::ostringstream ss;
+
+    ss << std::setw(k) << std::setfill('0') << n; // note '0'! (C char?)
+    return ss.str();
+}
+
+string Format::ElapsedTime(long seconds) {
+    long h, m, s;
+
+    h = seconds/3600;
+    m = (seconds - h*3600)/60;
+    s = (seconds - h*3600 - m*60);
+
+    return PadZeros(h, 2) + ":" + PadZeros(m, 2) + ":" + PadZeros(s, 2);
+}
