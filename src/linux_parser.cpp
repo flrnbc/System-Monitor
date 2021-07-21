b5b7685a742c773ff66eda6635739b91b1204f31
Florian Beck <flrn.beck@gmail.com>
2021-07-21 13:15:16 +0200

    Add Command(pid).

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -188,9 +188,17 @@ int LinuxParser::RunningProcesses() {
     return std::stoi(GetValue(kProdDirectory + kStatFilename, "procs_running"));
 }
 
-// TODO: Read and return the command associated with a process
+// DONE: Read and return the command associated with a process
 // REMOVE: [[maybe_unused]] once you define the function
-string LinuxParser::Command(int pid[[maybe_unused]]) { return string(); }
+string LinuxParser::Command(int pid) {
+    string cmd;
+    std::ifstream filestream(kProdDirectory + std::to_string(pid) + kCmdlineFilename);
+
+    if (filestream.is_open()) {
+        std::getline(filestream, cmd);
+    }
+    return cmd;
+}
 
 // DONE: Read and return the memory used by a process
 // REMOVE: [[maybe_unused]] once you define the function
