d2024eb477248c8f9d9eb1b309a9b48715aeab51
Florian Beck <flrn.beck@gmail.com>
2021-07-28 20:00:16 +0200

    Test wider 'command' column

--- src/ncurses_display.cpp
+++ src/ncurses_display.cpp
@@ -60,7 +60,7 @@ void NCursesDisplay::DisplayProcesses(std::vector<Process>& processes,
   int const cpu_column{16};
   int const ram_column{26};
   int const time_column{35};
-  int const command_column{46};
+  int const command_column{64};
   wattron(window, COLOR_PAIR(2));
   mvwprintw(window, ++row, pid_column, "PID");
   mvwprintw(window, row, user_column, "USER");
@@ -72,7 +72,7 @@ void NCursesDisplay::DisplayProcesses(std::vector<Process>& processes,
   for (int i = 0; i < n; ++i) {
     // Clear the line
     mvwprintw(window, ++row, pid_column, (string(window->_maxx-2, ' ').c_str()));
-    
+
     mvwprintw(window, row, pid_column, to_string(processes[i].Pid()).c_str());
     mvwprintw(window, row, user_column, processes[i].User().c_str());
     float cpu = processes[i].CpuUtilization() * 100;
